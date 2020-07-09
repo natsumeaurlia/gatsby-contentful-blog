065eea625e67cd35a1c7da578249e89ceb65031d
natsume0718 <moto718program@gmail.com>
2020-07-09 14:51:40 +0900

    [fix] バグ修正

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,8 @@ const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="404: Not Found" />
+    <Layout>
+      <SEO title="404: Not Found" location={location} />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
