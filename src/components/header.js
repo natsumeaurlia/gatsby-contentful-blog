93cf371d2e20068616454f79e053bcad72e5c273
natsume0718 <moto718program@gmail.com>
2020-07-10 00:42:48 +0900

    [update] ヘッダーにアイコン表示と画像取得コンポーネント作成

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import React from "react"
 import { Nav, Navbar } from "react-bootstrap"
 import { Link, useStaticQuery, graphql } from "gatsby"
 
+import Image from "./image"
+
 const Header = props => {
   const { site } = useStaticQuery(
     graphql`
@@ -20,13 +22,22 @@ const Header = props => {
     `
   )
   return (
-    <Navbar bg="white" expand="lg">
-      <Navbar.Brand href="/">{site.siteMetadata.title}</Navbar.Brand>
+    <Navbar fixed="top" bg="light" className="py-2 shadow-sm" expand="lg">
+      <Navbar.Brand href="/">
+        <div className="d-flex">
+          <Image
+            filename="icon.png"
+            className="mr-2"
+            style={{ width: "30px", height: "30px" }}
+          />
+          {site.siteMetadata.title}
+        </div>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/">
-            Home
+          <Nav.Link className="px-4" as={Link} to="/contact">
+            お問い合わせ
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
