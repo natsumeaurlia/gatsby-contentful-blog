b0d4816d56986db182e2e2eebfd58158af9288e0
natsume0718 <moto718program@gmail.com>
2021-02-27 15:56:54 +0900

    update sidebar

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "./header"
 import Footer from "./footer"
 import Sidebar from "./sidebar"
+import SNS from "./sns"
 
 import { Container, Col, Row } from "react-bootstrap"
 
@@ -11,11 +12,15 @@ const Layout = ({ children }) => {
       <Header />
       <Container fluid className="mt-5">
         <Row>
-          <Col lg={1}></Col>
+          <Col lg={1} id="sticky-sidebar">
+            <div class="sticky-top d-none d-lg-block">
+              <SNS className="d-flex flex-lg-column" />
+            </div>
+          </Col>
           <Col lg={8} md={12}>
             {children}
           </Col>
-          <Col lg={3} className="mt-3 mt-lg-0">
+          <Col lg={3} md={12} className="mt-3 mt-lg-0">
             <Sidebar />
           </Col>
         </Row>
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -37,53 +37,37 @@ const Sidebar = () => {
   const posts = data.allContentfulBlogPost.edges
   const categories = data.allContentfulCategory.edges
   return (
-    <Container>
-      <Row>
-        <div className="col-lg-12">
-          <Card className={`${Styles.recent_posts} border-0`}>
-            <Card.Body>
-              <Card.Title className={Styles.sidebar_heading}>
-                最近の投稿
-              </Card.Title>
-              <Card.Text>
-                <Nav>
-                  {posts.map(({ node }) => (
-                    <Nav.Link key={node.id} as={Link} to={`/post/${node.slug}`}>
-                      <h5 className={Styles.head}>{node.title}</h5>
-                      <span className={Styles.date}>
-                        {node.publishedDateJP}
-                      </span>
-                    </Nav.Link>
-                  ))}
-                </Nav>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div className="col-lg-12">
-          <Card className={Styles.sidebar_item + " border-0"}>
-            <Card.Body>
-              <Card.Title className={Styles.sidebar_heading}>
-                カテゴリー
-              </Card.Title>
-              <Card.Text>
-                <Nav className={Styles.categories}>
-                  {categories.map(({ node }) => (
-                    <Nav.Link
-                      key={node.id}
-                      as={Link}
-                      to={`/category/${node.slug}`}
-                    >
-                      {node.title}
-                    </Nav.Link>
-                  ))}
-                </Nav>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-      </Row>
-    </Container>
+    <React.Fragment>
+      <Card className={`${Styles.recent_posts} border-0`}>
+        <Card.Body>
+          <Card.Title className={Styles.sidebar_heading}>最近の投稿</Card.Title>
+          <Card.Text>
+            <Nav>
+              {posts.map(({ node }) => (
+                <Nav.Link key={node.id} as={Link} to={`/post/${node.slug}`}>
+                  <h5 className={Styles.head}>{node.title}</h5>
+                  <span className={Styles.date}>{node.publishedDateJP}</span>
+                </Nav.Link>
+              ))}
+            </Nav>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className={Styles.sidebar_item + " border-0"}>
+        <Card.Body>
+          <Card.Title className={Styles.sidebar_heading}>カテゴリー</Card.Title>
+          <Card.Text>
+            <Nav className={Styles.categories}>
+              {categories.map(({ node }) => (
+                <Nav.Link key={node.id} as={Link} to={`/category/${node.slug}`}>
+                  {node.title}
+                </Nav.Link>
+              ))}
+            </Nav>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </React.Fragment>
   )
 }
 
