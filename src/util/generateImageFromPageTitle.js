4ac6f333e7a19118f8733f37070f0f291d730a1e
natsume0718 <moto718program@gmail.com>
2021-04-04 20:58:07 +0900

    add constant background image url

--- src/util/generateImageFromPageTitle.js
+++ src/util/generateImageFromPageTitle.js
@@ -6,6 +6,13 @@ if (process.env.ENVIRONMENT !== "production") {
     dotenv.config()
 }
 
+const bg = [
+    'https://ntm-images.s3-ap-northeast-1.amazonaws.com/flower-3175428_1280.jpg',
+    'https://ntm-images.s3-ap-northeast-1.amazonaws.com/lime-2481346_1280.jpg',
+    'https://ntm-images.s3-ap-northeast-1.amazonaws.com/ogp_base.jpg',
+    'https://ntm-images.s3-ap-northeast-1.amazonaws.com/scott-webb-hDyO6rr3kqk-unsplash.jpg',
+    'https://ntm-images.s3-ap-northeast-1.amazonaws.com/water-1330252_640.jpg',
+]
 
 module.exports = async (pages, getCache, createNode, createNodeId, reporter, cache) => {
     const featureImages = new Map();
@@ -13,7 +20,10 @@ module.exports = async (pages, getCache, createNode, createNodeId, reporter, cac
     for (page of pages) {
         const { node } = page;
         const icons = node.icons;
-        let url = `${process.env.OPEN_GRAPH_GENERATE_API}${encodeURIComponent(node.title)}.png?md=1&fontSize=100px&background&fontColor=#777`
+        const random = Math.floor(Math.random() * bg.length - 1);
+        const image = bg[random] ? bg[random] : '';
+
+        let url = `${process.env.OPEN_GRAPH_GENERATE_API}${encodeURIComponent(node.title)}.png?md=1&fontSize=100px&background=${image}&fontColor=${encodeURIComponent('#696969')}`
         if (icons && icons.values) {
             for (icon of icons.values) {
                 url = url + '&icons=' + icon;
